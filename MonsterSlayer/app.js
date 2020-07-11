bf93ca1ea9f3090791190676dd8ee4d5f3d05c97
sanjeetboora <sanjeet.boora97@gmail.com>
2020-07-11 15:05:29 +0530

    implementing attack method

--- MonsterSlayer/app.js
+++ MonsterSlayer/app.js
@@ -7,9 +7,41 @@ new Vue({
     },
     methods: {
         startGame: function(){
-            this.gameIsRunning = true,
-            this.playerHealth = 100,
-            this.monsterHealth = 100
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+        },
+        attack: function(){
+            var min = 3;
+            var max = 10;
+            var damage = Math.max(Math.floor(Math.random() * max)+1, min);
+            this.monsterHealth -= damage;
+
+            if(this.monsterHealth <= 0){
+                alert("You Won!!!");
+                this.gameIsRunning = false;
+                return;
+            }
+
+            min=5;
+            max=12;
+            damage = Math.max(Math.floor(Math.random() * max)+1, min);
+            this.playerHealth -= damage;
+            
+            if(this.playerHealth <= 0){
+                alert("You Lost!!!");
+                this.gameIsRunning = false;
+                return;
+            }
+        },
+        specialAttack: function(){
+
+        },
+        heal: function(){
+
+        },
+        giveUp: function(){
+
         }
     }
 });
